18a7c513959572738ac9d8a9cc19da1f84de8146
Renjie Xie <freedomxier@hotmail.com>
2015-02-05 00:11:49 -0500

    Add homepage and about me

---
+++ about/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+  	<link rel="stylesheet" href="../css/nav.css">
+	<title>About Me</title>
+</head>
+<body>
+<div id="nav_wrapper">
+	<ul id="nav">
+		<li><a href='#'><span>Home</span></a></li>
+		<li><a href="#"><span>Restaurant</span></a></li>
+		<li><a href="about"><span>About</span></a></li>
+		
+	</ul>
+</div>
+<div id="intro">
+	<p>This is the website use to provide the paltform for restaurant to put their ordering service online</p>
+
+</div>
+<div id="mail">
+	<a href="mailto:freedomxier@hotmail.com">Contact Us</a>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+  	<link rel="stylesheet" href="css/nav.css">
+	<title>The Online Food Order System</title>
+</head>
+<body>
+<div id="nav_wrapper">
+	<ul id="nav">
+		<li><a href='#'><span>Home</span></a></li>
+		<li><a href="#"><span>Restaurant</span></a></li>
+		<li><a href="about"><span>About</span></a></li>
+		
+	</ul>
+</div>
+<p>This is produced for restaurants to provide online food menu<p>
+
+</body>
+</html>
